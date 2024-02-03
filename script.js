e411df7701b5676369e7b03403085a12bd3e7356
Alisson <100442612+alessaocarvalho@users.noreply.github.com>
2024-02-03 14:27:55 -0300

    Alterar tamnho da caixa

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const imagemVisualizacao = document.getElementById('imagem-visualizacao');
+const tituloProduto = document.getElementById('titulo-produto');
 
 const verdeCispreste = {
     nome: 'Verde-cipreste',
@@ -33,10 +34,28 @@ const opcoesCores = [
     rosaClaro
 ]
 
+const opcoesTamanho = [
+    '41 mm',
+    '45 mm'
+]
+
 let imagemSelecionada = 1;
+let tamanhoSelecionado = 1;
 
 function trocarImagem() {
     const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id;
     imagemSelecionada = idOpcaoSelecionada.charAt(0);
     imagemVisualizacao.src = './imagens/opcoes-cores/imagens-azul-inverno/imagem-' + imagemSelecionada + '.jpeg';
+}
+
+function trocarTamanho() {
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-tamanho"]:checked').id;
+    tamanhoSelecionado = idOpcaoSelecionada.charAt(0);
+    tituloProduto.innerText = 'Pulseira loop esportiva azul-inverno para caixa de ' + opcoesTamanho[tamanhoSelecionado];
+    if (opcoesTamanho[tamanhoSelecionado] === '41 mm') {
+        imagemVisualizacao.classList.add('caixa-pequena');
+    }
+    else {
+        imagemVisualizacao.classList.remove('caixa-pequena');
+    }
 }
